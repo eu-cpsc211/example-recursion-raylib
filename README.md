c2b1804670e53478b021e8ad499a4d6e738d71d2
victorbush <victorbush@gmail.com>
2025-04-10 14:31:54 -0500

    Finish example code.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Recursion Example with raylib
+
+This is an example of recursive drawing with raylib.
+
+The program draws a series of nested triangles using recursion. The drawing below illustrates the progression of drawing for each level of recursion (specified by `n`).
+
+![](./docs/recursion-levels.svg)
+
+Each nested triangle is rotated slightly such that its corners are shifted one-third the distance along the parent triangle's edge.
+
+![](./docs/one-third.svg)
--- src/main.cpp
+++ src/main.cpp
@@ -20,22 +20,59 @@ const int WINDOW_HEIGHT = 600;
 App state
 ----------------------------------------------------------------------------- */
 
+int g_depth = 0; // Depth of recursion
+
 /* ----------------------------------------------------------------------------
 Functions
 ----------------------------------------------------------------------------- */
 
-void DrawDemoTriangle(Vector2 a, Vector2 b, Vector2 c)
+/**
+ * Draw a triangle with a recursive interior triangle. Vertices of the triangle
+ * must be in counter-clockwise order.
+ * @param a First vertex of the triangle.
+ * @param b Second vertex of the triangle.
+ * @param c Third vertex of the triangle.
+ * @param n Number of recursion levels left to go.
+ */
+void DrawDemoTriangle(Vector2 a, Vector2 b, Vector2 c, int n)
 {
-    DrawTriangle(a, b, c, BLUE);
+    DrawTriangle(a, b, c, n % 2 ? WHITE : BLUE);
+
+    if (n <= 0)
+        return;
+
+    // Find b2
+    Vector2 b2 = Vector2Subtract(a, b);
+    b2 = Vector2Scale(b2, 1 / 3.0f);
+    b2 = Vector2Add(b, b2);
+
+    // Find a2
+    Vector2 a2 = Vector2Subtract(c, a);
+    a2 = Vector2Scale(a2, 1 / 3.0f);
+    a2 = Vector2Add(a, a2);
+
+    // Find c2
+    Vector2 c2 = Vector2Subtract(b, c);
+    c2 = Vector2Scale(c2, 1 / 3.0f);
+    c2 = Vector2Add(c, c2);
+
+    // Recursive call to render interior triangle
+    DrawDemoTriangle(a2, b2, c2, n - 1);
 }
 
+/**
+ * Draws the example triangle recursion.
+ */
 void DrawDemo()
 {
-    Vector2 a = { WINDOW_WIDTH / 2.0, 0 };  // Top
-    Vector2 b = { 0, WINDOW_HEIGHT }; // Lower left
-    Vector2 c = { WINDOW_WIDTH, WINDOW_HEIGHT }; // Lower right
+    DrawText("Demo", 10, 10, 14, BLACK);
+    DrawText(std::to_string(g_depth).c_str(), 10, 25, 14, BLACK);
 
-    DrawDemoTriangle(a, b, c);
+    Vector2 a = {WINDOW_WIDTH / 2.0, 0};       // Top
+    Vector2 b = {0, WINDOW_HEIGHT};            // Lower left
+    Vector2 c = {WINDOW_WIDTH, WINDOW_HEIGHT}; // Lower right
+
+    DrawDemoTriangle(a, b, c, g_depth);
 }
 
 /**
@@ -51,7 +88,7 @@ int main(void)
     SetConfigFlags(FLAG_WINDOW_HIGHDPI);
 
     // Setup the application window
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Fractals");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Recursion");
 
     // Set game to run at 60 frames-per-second
     SetTargetFPS(60);
@@ -66,7 +103,12 @@ int main(void)
         Update frame
         ------------------------------------------------------------------------- */
 
-        // TODO : Update stuff here.
+        if (IsKeyPressed(KEY_LEFT)) {
+            g_depth = max(0, g_depth - 1);
+        }
+        else if (IsKeyPressed(KEY_RIGHT)) {
+            g_depth++;
+        }
 
         /* ------------------------------------------------------------------------
         Draw frame
@@ -75,7 +117,6 @@ int main(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        // TODO : Draw stuff here.
         DrawDemo();
 
         EndDrawing();
